99294651ddbd675b069711881683c377b584a18b
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-09-26 16:53:47 +0200

    Rotate String

---
+++ week2/rotatestring.js
@@ -0,0 +1,48 @@
+/*
+ROTATE STRING
+
+
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+
+
+abc -> cba
+
+
+
+
+
+
+*/
+
+var rotateString = function (s, goal) {
+  let g = "";
+  const arr = s.split("");
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let removed = arr.shift();
+    let pushed = arr.push(removed);
+
+    if (arr.join("") == goal) {
+      console.log(arr.join(""));
+      break;
+    }
+  }
+
+  return arr.join("") === goal;
+};
+
+console.log(rotateString("abcde", "cdeab"));
