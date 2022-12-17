a4e6f063d5e3e3149d44e865b24a483cacf40bc9
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-17 18:37:02 +0300

    Add example and explanation of Running sum challenge

--- december/runningSum.js
+++ december/runningSum.js
@@ -1,16 +1,15 @@
 var runningSum = function (nums) {
   // nums[1] + nums[2]
   let first = 0;
-  let second = 1;
 
   const sum = (ns) => ns.reduce((sum, curr) => sum + curr, nums[0]);
 
   //   console.log(nums.slice(0, second));
 
-  console.log(sum(nums.slice(first, 0)));
+  console.log(sum(nums.slice(first, 1)));
 
   return nums.map((n, i) => sum(nums.slice(first, i)));
 };
 
 // console.log(runningSum([1, 2, 3, 4, 0])); // 1 3 6 10
-console.log(runningSum([1, 1, 1, 1, 1])); // 1 3 6 10
+console.log(runningSum([1, 2, 3, 4])); // [1,3,6,10]
