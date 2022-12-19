b21a3f556b493f1279f71be1f7bb995c7f8db14f
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-19 20:21:52 +0300

    Maximum Subarray -

--- december/2-week-DS/maxSubArray.js
+++ december/2-week-DS/maxSubArray.js
@@ -1,15 +1,13 @@
 var maxSubArray = function (nums) {
-  // KADANE'S ALGOTHRIM
-
-  // initialize max sum to 0
-  let maxSum = 0;
+  // first element is it's contigous subArray so we set maxSum to index 0
+  let maxSum = nums[0];
 
   // set current sum to be the max sum
   let currentSum = maxSum;
 
-  for (let i = 0; i < nums.length; i++) {
+  // set a pointer at index 1 and iterate over the array
+  for (let i = 1; i < nums.length; i++) {
     // currentSum is equal to the max of the (current iteration(nums[i]) + the previous currentSum ) or (the current iteration)
-
     currentSum = Math.max(nums[i] + currentSum, nums[i]);
     maxSum = Math.max(currentSum, maxSum);
   }
@@ -17,6 +15,5 @@ var maxSubArray = function (nums) {
 };
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
-
 console.log(maxSubArray([-2, 1, -3, 4]));
 console.log(maxSubArray([5, 4, -1, 7, 8]));
