463b7e9c34d46d61a4371b919a12ecc899cda0fa
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-20 18:54:05 +0300

    Merge sorted array - solution

--- december/2-week-DS/mergeSortedArray.js
+++ december/2-week-DS/mergeSortedArray.js
@@ -3,3 +3,7 @@ var merge = function (nums1, m, nums2, n) {
   nums1.push(...nums2);
   nums1.sort((a, b) => a - b);
 };
+console.log(merge([0], 0, [1], 1)); // [1]
+console.log(merge([0, 0, 0, 0, 0], 0, [1, 2, 3, 4, 5], 5));
+console.log(merge([1, 2, 4, 5, 6, 0], 5, [3], 1)); // [1,2,3,4,5,6]
+console.log(merge([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3)); // [-1,0,0,1,2,2,3,3,3]
