716f4703bdd763451b1dad8c02646f2e48b38fe2
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-21 13:45:53 +0300

    Intersection of Two Arrays II - description

--- december/2-week-DS/intersection2arrys.js
+++ december/2-week-DS/intersection2arrys.js
@@ -1,3 +1,33 @@
+/*
+Given two integer arrays nums1 and nums2, 
+return an array of their intersection. 
+Each element in the result must appear as many times as it shows in both arrays ??
+===¡ if the number 2 appears 2 times in both arrays then you should return an array of 2 2s
+and you may return the result in any order.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+
+
+Attempt 1
+concat both array and filter out the none duplicates -/-
+===> does not work cause nums1 could have one number 2 times that isnt in nums2
+
+
+Attempt 2
+for loop and an if statement that stores dups in a new array
+
+
+*/
+
 var intersect = function (nums1, nums2) {
   const shortestArray = nums1.length < nums2.length ? nums1 : nums2;
   const longestArray = nums2.length > nums1.length ? nums2 : nums1;
