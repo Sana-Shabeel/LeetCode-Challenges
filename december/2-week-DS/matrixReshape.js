9d7b14d2b381ab15c0ae8fc531a30d59b1a5cf37
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-22 17:23:48 +0300

    Reshape the Matrix - solution

--- december/2-week-DS/matrixReshape.js
+++ december/2-week-DS/matrixReshape.js
@@ -7,3 +7,28 @@
 
 
 */
+
+var matrixReshape = function (mat, r, c) {
+  const flattendArray = mat.flat();
+
+  if (mat.length * mat[0].length != r * c) return mat;
+
+  const answer = [];
+
+  for (let i = 0; i < flattendArray.length; i += c) {
+    answer.push(flattendArray.slice(i, i + c));
+  }
+
+  return answer;
+};
+
+console.log(
+  matrixReshape(
+    [
+      [1, 2],
+      [3, 4],
+    ],
+    1,
+    4
+  )
+);
