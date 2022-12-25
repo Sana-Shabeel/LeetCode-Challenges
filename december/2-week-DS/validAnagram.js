ad6e51cf2267e599a4d7a0fea4ea5cb6e7429042
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-25 17:10:14 +0300

    Valid Anagaram - solution

---
+++ december/2-week-DS/validAnagram.js
@@ -0,0 +1,13 @@
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  for (let i = 0; i < t.length; i++) {
+    const char = t[i];
+
+    if (splitStr.includes(char)) {
+      splitStr.splice(splitStr.indexOf(char), 1);
+    }
+  }
+
+  return splitStr.length === 0 ? true : false;
+};
