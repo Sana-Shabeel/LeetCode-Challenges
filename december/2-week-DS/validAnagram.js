9d8493f567eb3333440c5ae7947ec5e5508a4d3d
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-25 17:12:09 +0300

    Valid Anagaram - faster runtime solution

--- december/2-week-DS/validAnagram.js
+++ december/2-week-DS/validAnagram.js
@@ -11,5 +11,17 @@ var isAnagram = function (s, t) {
 
   return splitStr.length === 0 ? true : false;
 };
+
+// a faster runtime solution
+
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  const sortS = s.split("").sort().join("");
+  const sortT = t.split("").sort().join("");
+
+  return sortS === sortT;
+};
+
 console.log(isAnagram("anagram", "nagaram"));
 console.log(isAnagram("aacc", "ccac"));
