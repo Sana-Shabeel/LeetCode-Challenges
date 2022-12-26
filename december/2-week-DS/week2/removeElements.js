9b885e9e0f1b2b6723c83b99efdfc37a8fd2e7dd
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-26 20:50:24 +0300

    Remove Linked List Elements - add comments

--- december/2-week-DS/week2/removeElements.js
+++ december/2-week-DS/week2/removeElements.js
@@ -18,22 +18,33 @@ Input: head = [7,7,7,7], val = 7
 Output: []
 
 */
-
 var removeElements = function (head, val) {
-  let dummy = new ListNode(-1);
+  let dummy = new Node(-1);
+
+  // chain dummy to head, so dummy.next would be head
   dummy.next = head;
+
+  // dummy here is -1 but dummy.next would be head
   let prev = dummy;
   let current = head;
 
   while (current !== null) {
     if (current.val === val) {
+      // move previous to to the node after the current node
       prev.next = current.next;
+      // current node will be where previous node is
+      // fx [1,2,3,4]  if current node was at 2 and prev node was at 1, prev node would now be at 3 and current node would progress normally moving it to the next node
       current = current.next;
     } else {
+      // otherwise previous node would be tailing current node
       prev = current;
+
+      // and current node will move normally
       current = current.next;
     }
   }
 
+  // since prev node is now now at null we will return dummy.next,
   return dummy.next;
 };
+console.log(removeElements(a, 6));
