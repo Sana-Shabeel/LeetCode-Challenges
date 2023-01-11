c9cd91cc13f6c6f8f4087332c7e874247c06b72e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-11 21:27:31 +0300

    Climbing Stairs - solution

---
+++ 2023/january/climbStairs.js
@@ -0,0 +1,12 @@
+var climbStairs = function (n) {
+  let one = 1;
+  let two = 1;
+
+  for (let i = n; i > 0; i--) {
+    let temp = one;
+    one = one + two;
+    two = temp;
+  }
+
+  return two;
+};
