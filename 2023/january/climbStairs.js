636b0aafffede6cdf192107c3646d5329a63c63c
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-11 21:28:16 +0300

    Climbing Stairs - description

--- 2023/january/climbStairs.js
+++ 2023/january/climbStairs.js
@@ -1,3 +1,13 @@
+/*
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+
+*/
+
 var climbStairs = function (n) {
   let one = 1;
   let two = 1;
