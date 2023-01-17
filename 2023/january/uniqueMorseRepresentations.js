df08fe618bf468760732bab5bd0c99f5be1aba4f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-17 14:37:11 +0300

    Unique Morse Code Words - description

---
+++ 2023/january/uniqueMorseRepresentations.js
@@ -0,0 +1,9 @@
+/*
+Unique Morse Code Words
+
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+*/
