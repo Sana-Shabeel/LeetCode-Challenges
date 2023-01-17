4cdbccb72db2cea9d9622f3441c69a90718839b1
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-17 14:37:30 +0300

    Unique Morse Code Words - example

--- 2023/january/uniqueMorseRepresentations.js
+++ 2023/january/uniqueMorseRepresentations.js
@@ -6,4 +6,15 @@ Given an array of strings words where each word can be written as a concatenatio
 For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
 Return the number of different transformations among all words we have.
 
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+
 */
