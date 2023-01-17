93b603b1cfac9865b5d7b329952f84f1725416f2
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-17 14:38:03 +0300

    Unique Morse Code Words - MORSE_CODE

--- 2023/january/uniqueMorseRepresentations.js
+++ 2023/january/uniqueMorseRepresentations.js
@@ -18,3 +18,32 @@ Explanation: The transformation of each word is:
 There are 2 different transformations: "--...-." and "--...--.".
 
 */
+
+var MORSE_CODE = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+};
