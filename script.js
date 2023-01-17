b41287c84c7f5966e8ec076adbd0144299788c47
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-17 14:39:32 +0300

    clean sript file

--- script.js
+++
@@ -1,9 +0,0 @@
-const maxArray = (numbers) => {
-  if (numbers.length === 0) return 0;
-  const sum = numbers.reduce((a, b) => a + b, 0);
-
-  return sum / numbers.length;
-};
-
-console.log(maxArray([1, 2, 3]));
-console.log(maxArray([]));
