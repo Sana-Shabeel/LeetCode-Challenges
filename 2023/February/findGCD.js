b7947ae01b15f5f0e83d20e4d1d729b563fa7baa
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-01 13:53:01 +0300

    Find Greatest Common Divisor of Array - example

--- 2023/February/findGCD.js
+++ 2023/February/findGCD.js
@@ -5,4 +5,15 @@ Given an integer array nums, return the greatest common divisor of the smallest 
 
 The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
 
+Example 1:
+
+Input: nums = [2,5,6,9,10]
+Output: 2
+Explanation:
+The smallest number in nums is 2.
+The largest number in nums is 10.
+The greatest common divisor of 2 and 10 is 2.
+Example 2:
+
+
 */
