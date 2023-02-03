e1abb0fa026f63f2429f3a4342acd4e36a71277e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-03 08:29:47 +0300

    Plus One - alt solution

--- 2023/February/PlusOne.js
+++ 2023/February/PlusOne.js
@@ -29,3 +29,20 @@ var plusOne = function (digits) {
 console.log(plusOne([1, 2, 3]));
 console.log(plusOne([9]));
 console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+
+// c
+
+function increment(digits) {
+  for (let i = digits.length - 1; i > -1; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+  console.log(digits);
+
+  digits.unshift(1); // add 1 to most significant digit
+  return digits;
+}
