e4bee4cd940b1be4b53cc1f2d18743ab6539d38a
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-03 08:10:57 +0300

    Plus One - examples

--- 2023/February/PlusOne.js
+++ 2023/February/PlusOne.js
@@ -7,5 +7,17 @@ You are given a large integer represented as an integer array digits, where each
 Increment the large integer by one and return the resulting array of digits.
 
 
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+
 
 */
