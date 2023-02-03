8ee24f213de4b1d0a5274cfd99052f595614978e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-03 08:11:44 +0300

    Plus One - bigInt solution

--- 2023/February/PlusOne.js
+++ 2023/February/PlusOne.js
@@ -26,3 +26,6 @@ var plusOne = function (digits) {
 
   return toBigInt.toString().split("").map(Number);
 };
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([9]));
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
