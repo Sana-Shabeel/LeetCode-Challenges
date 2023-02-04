5ef46fa038a16ceb7d1d83388948288fee0979c1
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-04 13:20:33 +0300

    Add binary - alt solution

--- 2023/February/addBinary.js
+++ 2023/February/addBinary.js
@@ -34,3 +34,12 @@ var addBinary = function (a, b) {
   // let numB = bToD(b);
   // return (numA + numB).toString(2);
 };
+
+// Alt solution
+
+var addBinary = function (a, b) {
+  // we used BigInt() to convert binary to Integer. Syntax is BigInt(0b11) here `0b` is prefix and `11` is binary number
+  let sum = BigInt(`0b${a}`) + BigInt(`0b${b}`);
+  // sum is converted back to string using toString(2), here 2 is radix argument, so toString(2) converts to binary
+  return sum.toString(2);
+};
