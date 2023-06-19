aeb0d34d4166e8acd0cbabb72c2778f308c1b5d2
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-06-19 17:21:30 +0300

    Word pattern

--- 2023/June/wordPattern.js
+++ 2023/June/wordPattern.js
@@ -1,4 +1,37 @@
 /*
 
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+ 
+
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
 
 */
+
+var wordPattern = function (pattern, s) {
+  let words = s.split(" ");
+  let map = new Map();
+  let set = new Set();
+  if (words.length !== pattern.length) return false;
+  for (let i = 0; i < words.length; i++) {
+    if (!map.has(pattern[i])) {
+      if (set.has(words[i])) return false;
+      map.set(pattern[i], words[i]);
+      set.add(words[i]);
+    } else {
+      if (map.get(pattern[i]) !== words[i]) return false;
+    }
+  }
+  return true;
+};
+
+console.log(wordPattern(["abba", "dog cat cat dog"]));
